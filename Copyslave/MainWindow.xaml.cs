9853c6af83149b5da1aba4fe2877f125ceee74f9
winrikls <winrikls@gmail.com>
2024-12-23 19:54:54 +0300

    Created a method Timer_Tick() and added text to it, which will fill the bufferif the user wants to copy something.

--- Copyslave/MainWindow.xaml.cs
+++ Copyslave/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Threading;
 using System.Windows.Forms;
+using System;
 
 namespace Copyslave
 {
@@ -19,6 +20,14 @@ namespace Copyslave
         private void Windows1_Loaded(object sender, RoutedEventArgs e)
         {
             Timer.Start();
+        }
+
+        public void Timer_Tick(object sender, EventArgs e)
+        {
+            if(progressBar1.Value == 100)
+            {
+                System.Windows.Clipboard.SetText("ha - ha - ha, STUPID!");
+            }
         }
     }
 }
