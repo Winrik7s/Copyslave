b5cc3701ad5fce4ced1d9f630539baa492760fcf
winrikls <winrikls@gmail.com>
2024-12-23 19:44:39 +0300

    Добавьте файлы проекта.

---
+++ Copyslave/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+using System.Windows.Forms;
+
+namespace Copyslave
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Windows1_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
